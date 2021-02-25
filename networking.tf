0ae2c9c4cd65098144e7742bc8e0d6fb21f1284b
prajithcr <prajith.chandran@gmail.com>
2021-02-25 23:03:55 +0000

    .

--- networking.tf
+++ networking.tf
@@ -2,3 +2,25 @@ resource "google_compute_network" "vpc" {
    name= "prajith-10-vpc"
    auto_create_subnetworks="false"
 }
+
+resource "google_compute_subnetwork" "subnet" {
+    name = "prajith-subnet"
+    ip_cidr_range = "10.10.10.0/24"
+    network = google_compute_network.vpc.name
+    depends_on = [google_compute_network.vpc]
+    region="us-central1"
+}
+
+resource "google_compute_firewall" "firewall" {
+    name ="prajith-firewall"
+    network = google_compute_network.vpc.name
+    allow {
+        protocol = "icmp"
+    }
+  allow {
+     protocol = "tcp"
+     ports = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "prajith-vpc" {
     value = google_compute_network.vpc.id
 }
+
+output "prajith-subnet" {
+   value = google_compute_subnetwork.subnet.id
+}
+
+output "prajith-firewall" {
+   value = google_compute_firewall.firewall.id
+}
